0cb6d226f815fd6de8893c63994754678ac9ae66
llee-125 <llee@berkeley.edu>
2020-05-29 18:56:54 -0700

    Updated after Tutor session. Needs readme cleanup and more questions.

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-const questions = [
+// const questions = [
 
-];
+// ];
 
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
-function init() {
+// function init() {
 
-}
+// }
 
-init();
+// init();
+
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+var generate = require("./utils/generateMarkdown");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project title?",
+    },
+    {
+      type: "input",
+      name: "desc",
+      message: "What is the description of your project?",
+    },
+  ])
+  .then(function (data) {
+    console.log(data);
+
+    fs.writeFile("README.md", generate(data), function (err) {
+      if (err) {
+        return console.log("error");
+      }
+    });
+  });
+// }]);
+
+//   At least one badge
+// Project title
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions
+
+// User GitHub profile picture
+// User GitHub email]
